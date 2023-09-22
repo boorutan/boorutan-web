b1edf90dfb75940ec7f946ce440f0d8f94831346
apple <apple.mangoooo97@gmail.com>
2023-09-22 19:43:45 +0900

    add category and more

--- app/@modal/(.)i/[id]/page.tsx
+++ app/@modal/(.)i/[id]/page.tsx
@@ -1,16 +1,43 @@
 import ImageModal from "@/components/imagemodal"
 import {req} from "@/lib/fetch";
 
+const createMoeBooruCategory = (category: any[]): any => {
+    return category.reduce((acc, v)=> {
+        acc[v.Name] = v.Category
+        return acc
+    }, {})
+}
+
+const createDanBooruCategoryHelper = (tags: string, type: string) => {
+    return tags.split(" ").reduce((acc: any, v)=> {
+        acc[v] = type
+        return acc
+    }, {})
+}
+const createDanBooruCategory = (post: any) => {
+    return Object.assign(
+        createDanBooruCategoryHelper(post.tag_string_general, "0"),
+        createDanBooruCategoryHelper(post.tag_string_artist, "1"),
+        createDanBooruCategoryHelper(post.tag_string_copyright, "3"),
+        createDanBooruCategoryHelper(post.tag_string_character, "4"),
+        createDanBooruCategoryHelper(post.tag_string_meta, "5"),
+    )
+}
+
 const ImageModal_ = async ({ params: { id } }:{
     params: {
         id: number
     }
 }) => {
     const post = await req<any>(`/post/${id}`)
-    const tags = post.tags || post.tag_string
-    const category = await req<any>(`/category?tag=${tags}`, {
-        isResultArray: true
-    })
-    return <ImageModal category={category} post={post} />
+    const tags = post.tags
+    if (tags) {
+        const category = await req<any>(`/category?tag=${tags}`, {
+            isResultArray: true
+        })
+        return <ImageModal category={createMoeBooruCategory(category)} post={post} />
+    }
+    return <ImageModal category={createDanBooruCategory(post)} post={post} />
+
 }
 export default ImageModal_
--- components/imagemodal.tsx
+++ components/imagemodal.tsx
@@ -4,6 +4,23 @@ import { useCallback } from "react"
 import BooruImage, {BooruImageFromPost} from "@/components/booruImage";
 import {getOriginalUrl} from "@/lib/booru";
 
+const getCategoryData = (post: any, category: any, type: string, none: string) => {
+    const character = Object.keys(category).filter((v: any)=> category[v] == type)
+    if(character.length)
+        return character.reduce((acc: string, v: any, i: number)=> {
+            return i ? `${acc},${v}` : v
+        }, "")
+    return none
+}
+
+const getTitle = (post: any, category: any) => {
+    return getCategoryData(post, category, "4", "Original")
+}
+
+const getDescription = (post: any, category: any) => {
+    return getCategoryData(post, category, "3", "Original")
+}
+
 const ImageModal = ({post, category}:{
     post: any,
     category: any,
@@ -12,7 +29,7 @@ const ImageModal = ({post, category}:{
     const back = useCallback(() => {
         router.back()
     }, [router])
-    console.log(post);
+    console.log(post, category);
     return <div id={"modal"} onClick={back} style={{
         position: "fixed",
         top: 0,
@@ -21,20 +38,35 @@ const ImageModal = ({post, category}:{
         display: "flex",
         alignItems: "center",
         justifyContent: "center",
+        overflowY: "auto"
     }}>
         <div style={{
-            width: "100%",
-            height: "100%",
+            width: "100vw",
+            height: "100vh",
             backgroundColor: "#fff",
             display: "flex",
             justifyContent: "center",
-            alignItems: "center"
+            alignItems: "center",
+            position: "relative",
         }}>
             <BooruImageFromPost style={{
                 maxWidth: "80%",
-                height: "100%",
-                width: "auto"
+                height: "calc(100% - 32px)",
+                width: "auto",
+                //boxShadow: "rgba(149, 157, 165, 0.2) 0px 8px 24px"
             }} original post={post} />
+            <div style={{
+                position: "absolute",
+                bottom: 16,
+                left: 32,
+            }}>
+                <p style={{
+                    color: "#000"
+                }}>{getTitle(post, category)}</p>
+                <p style={{
+                    color: "#000"
+                }}>{getDescription(post, category)}</p>
+            </div>
         </div>
     </div>
 }
