b7e51292c72b6b64a74c5529b0f4ade1c1a2396e
apple <apple.mangoooo97@gmail.com>
2024-01-03 18:38:01 +0900

    Create object.ts

---
+++ lib/utils/object.ts
@@ -0,0 +1,29 @@
+export const mergeObject = <T extends  Object | undefined = any>(...objects: Array<T>) => {
+    return objects.filter((obj) => typeof obj == "object" && !!obj).reduce((acc: any, obj: any)=> {
+        Object.keys(obj as Object).map((k)=> {
+            if(
+                Object.keys(acc).includes(k)
+                && ![null, undefined].includes(acc[k])
+                && !obj[k]
+            ) return
+            acc[k] = obj[k]
+        })
+        return acc
+    },{})
+}
+
+export const mergeObjectForce = (...objects: Array<any>) => {
+    return objects.filter((obj) => typeof obj == "object" && !!obj).reduce((acc, obj)=> {
+        Object.keys(obj).map((k)=> {
+            acc[k] = obj[k]
+        })
+        return acc
+    },{})
+}
+
+export const optionObjectValue2String = (object: any) => {
+    return Object.keys(object).reduce((acc: any, k)=> {
+        acc[k] = object[k] || ""
+        return acc
+    }, {})
+}
