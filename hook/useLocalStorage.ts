bcfc60f5ce09c002158725632221e4ca87653524
apple <apple.mangoooo97@gmail.com>
2024-01-04 15:11:34 +0900

    Create useLocalStorage.ts

---
+++ hook/useLocalStorage.ts
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react"
+
+type Fn<T> = ((value: T)=> T)
+type V<T> = T
+
+export const useLocalStorage = <T>(key: string, init?: T): [T, (value: Fn<T> | V<T>)=> void] => {
+    const [state, setState] = useState<T>(init as any)
+    useEffect(()=> {
+        setState(()=> {
+            const value = localStorage.getItem(key)
+            return value ? JSON.parse(value) : init
+        })
+    },[])
+    return [state, (value: Fn<T> | V<T>)=> {
+        if(value instanceof Function) {
+            return setState((v)=> {
+                const c = value(v)
+                localStorage.setItem(key, JSON.stringify(c))
+                return c
+            })
+        }
+        localStorage.setItem(key, JSON.stringify(value))
+        setState(value)
+    }]
+}
